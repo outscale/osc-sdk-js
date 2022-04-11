2e3c9f9f0ebca917f5a454fd56d80eff1ee6044f
Jérôme Jutteau <jerome.jutteau@outscale.com>
2022-04-11 16:58:58 +0200

    First generation of osc-sdk-js
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

---
+++ Makefile
@@ -0,0 +1,138 @@
+API_VERSION=$(shell cat api_version)
+SDK_VERSION=$(shell cat sdk_version)
+USER_ID=$(shell id -u)
+GROUP_ID=$(shell id -g)
+OPENAPI_IMG=outscale/openapi-generator:ts-fetch-aws-v4
+SHELL := /bin/bash
+
+all: help
+
+.PHONY: help
+help:
+	@echo "help:"
+	@echo "- make gen   : regenerate SDK"
+	@echo "- make test  : run all tests"
+
+.PHONY: gen
+gen: clean osc-generate
+
+.PNONY: openapi-generator-help
+openapi-generator-help:
+	docker run -v $(PWD):/sdk --rm $(OPENAPI_IMG) config-help -g typescript-fetch
+
+.PHONY: osc-generate
+osc-generate: osc-api/outscale.yaml
+	@echo start generating SDK...
+	rm -rf .sdk || true
+	mkdir .sdk
+	docker run -v $(PWD):/sdk --rm $(OPENAPI_IMG) generate -i /sdk/osc-api/outscale.yaml -g typescript-fetch -c /sdk/gen.yml -o /sdk/.sdk --additional-properties=npmVersion=$(SDK_VERSION)
+	# Set default user agent including sdk version using reproductible sed.
+	docker run -v $(PWD):/sdk --rm $(OPENAPI_IMG) chown -R $(USER_ID).$(GROUP_ID) /sdk/.sdk
+	mv .sdk/src ./
+	@echo SDK generated
+	@echo testing SDK build...
+	@source ~/.nvm/nvm.sh; \
+	echo "nvm --version:"; \
+	nvm --version; \
+	echo "nvm install..."; \
+	nvm install; \
+	echo "nvm use..."; \
+	nvm use; \
+	echo "npm version..."; \
+	npm version; \
+	echo "npm install..."; \
+	npm install --local
+
+osc-api/outscale.yaml:
+	@echo getting osc-api description...
+	git clone https://github.com/outscale/osc-api.git && cd osc-api && git checkout -b $(API_VERSION) $(API_VERSION)
+
+.PHONY: clean
+clean:
+	@echo cleaning build artifacts...
+	rm -rf .sdk osc-api src node_modules dist || true
+
+.PHONY: test
+test: reuse-test examples-test regen-test
+	@echo all tests OK...
+
+.PHONY: reuse-test
+reuse-test:
+	@echo testing licensing with reuse...
+	docker run --rm --volume $(PWD):/data fsfe/reuse:0.11.1 lint
+
+.PHONY: examples-test
+examples-test: example-web-vms example-node-volumes
+	@echo examples are OK
+
+.PHONY: example-web-vms
+example-web-vms:
+	@echo testing examples/web-vms example...
+	@source ~/.nvm/nvm.sh; \
+	cd examples/web-vms; \
+	echo "nvm --version:"; \
+	nvm --version; \
+	echo "installing nvm..."; \
+	nvm install; \
+	echo "using nvm..."; \
+	nvm use; \
+	echo "npm version:"; \
+	npm version; \
+	echo "npm install..."; \
+	npm install --local --install-links
+
+.PHONY: example-node-volumes
+example-node-volumes:
+	@echo testing examples/node-volumes example...
+	@source ~/.nvm/nvm.sh; \
+	cd examples/node-volumes; \
+	echo "nvm version:"; \
+	nvm --version; \
+	echo "nvm install..."; \
+	nvm install; \
+	echo "nvm use..."; \
+	nvm use; \
+	echo "npm version:"; \
+	npm version; \
+	echo "npm install..."; \
+	npm install --local --install-links; \
+	echo "running node example..."; \
+	node ./src/index.js > /dev/null
+
+# try to regen, should not have any difference
+.PHONY: regen-test
+regen-test: gen
+	@echo SDK regeneration test...
+	git add src dist
+	git diff --cached -s --exit-code
+	git diff -s --exit-code
+
+# Used by bot to auto-release
+# GH_TOKEN and SSH_PRIVATE_KEY are needed
+.PHONY: auto-release
+auto-release: auto-release-cleanup osc-api-check release-check-duplicate release-build release-push release-pr
+	@echo OK
+
+.PHONY: auto-release-cleanup
+auto-release-cleanup:
+	rm -rf .auto-release-abort || true
+
+.PHONY: osc-api-check
+osc-api-check:
+	bash .github/scripts/osc-api-check.sh
+
+.PHONY: release-check-duplicate
+release-check-duplicate:
+	bash .github/scripts/release-check-duplicate.sh
+
+.PHONY: release-build
+release-build:
+	bash .github/scripts/release-build.sh
+
+.PHONY: release-push
+release-push:
+	bash .github/scripts/release-push.sh
+
+.PHONY: release-pr
+release-pr:
+	bash .github/scripts/release-pr.sh
---
+++ examples/web-vms/src/index.ts
@@ -0,0 +1,139 @@
+import * as osc from "outscale-api";
+
+export default function ShowVms() {
+    printResult("Quering Outscale API ...");
+
+    const accessKeyField = document.getElementById("accessKey") as HTMLInputElement;
+    if (!accessKeyField) {
+        console.error("Cannot getElementById(\"accessKey\")");
+        return;
+    }
+    const accessKey = accessKeyField.value;
+
+    const secretKeyField = document.getElementById("secretKey") as HTMLInputElement;;
+    if (!secretKeyField) {
+        console.error("Cannot getElementById(\"secretKey\")");
+        return;
+    }
+    const secretKey = secretKeyField.value;
+
+    const regionField = document.getElementById("region") as HTMLInputElement;;
+    if (!regionField) {
+        console.error("Cannot getElementById(\"region\")");
+        return;
+    }
+    const region = regionField.value;
+
+    getVms(accessKey, secretKey, region).then(vmsResult => {
+        if (typeof vmsResult == "string") {
+            printResult(vmsResult);
+            return;
+        }
+        printResult(vms2Html(vmsResult));
+    });
+}
+
+function printResult(content: string | HTMLDivElement) {
+    let result = document.getElementById("result");
+    if (!result) {
+        console.error("Cannot getElementById(\"result\")");
+        return;
+    }
+    result.textContent = '';
+    if (typeof content == "string") {
+        const querying = document.createElement('p');
+        querying.innerHTML = content;
+        result.appendChild(querying);
+    } else {
+        result.appendChild(content);
+    }
+}
+
+ async function getVms(accessKey: string, secretKey: string, region: string): Promise<Array<osc.Vm> | string> {
+    let config = new osc.Configuration({
+        basePath: "https://api." + region + ".outscale.com/api/v1",
+        awsV4SignParameters: {
+            accessKeyId: accessKey,
+            secretAccessKey: secretKey,
+            service: "api",
+            region: region,
+        }
+    });
+    let readParameters : osc.ReadVmsOperationRequest = {
+        readVmsRequest: {}
+    };
+
+    let api = new osc.VmApi(config)
+    return api.readVms(readParameters)
+    .then((res: osc.ReadVmsResponse | string) => {
+        if (typeof res == "string") {
+            return res;
+        }
+        if (res.vms === undefined || res.vms.length == 0) {
+            return "Listing suceeded but it seems you have no vm";
+        }
+        return res.vms;
+    }, (err_: any) => {
+        return "Error, bad credential or region?";
+    });
+}
+
+function vms2Html(vms: Array<osc.Vm>): HTMLDivElement {
+    const result = document.createElement('div');
+
+    const intro = document.createElement('p');
+    intro.innerHTML = "You have "+ vms.length + " Virtual Machines:";
+    result.appendChild(intro);
+
+    const table = document.createElement('table');
+    result.appendChild(table);
+
+    const header = document.createElement('tr');
+    table.appendChild(header);
+
+    const idHeader = document.createElement('th');
+    idHeader.innerHTML = "VmId";
+    header.appendChild(idHeader);
+
+    const stateHeader = document.createElement('th');
+    stateHeader.innerHTML = "State";
+    header.appendChild(stateHeader);
+
+    const typeHeader = document.createElement('th');
+    typeHeader.innerHTML = "VmType";
+    header.appendChild(typeHeader);
+
+    const perfHeader = document.createElement('th');
+    perfHeader.innerHTML = "Performance";
+    header.appendChild(perfHeader);
+
+    const publicIpHeader = document.createElement('th');
+    publicIpHeader.innerHTML = "PublicIp";
+    header.appendChild(publicIpHeader);
+
+    for (const vm of vms) {
+        const vmRow = document.createElement('tr');
+        table.appendChild(vmRow);
+
+        const vmId = document.createElement('td');
+        vmId.innerHTML = vm.vmId?.toString() ?? "Unknown";
+        vmRow.appendChild(vmId);
+
+        const state = document.createElement('td');
+        state.innerHTML = vm.state?.toString() ?? "Unknown";
+        vmRow.appendChild(state);
+
+        const vmType = document.createElement('td');
+        vmType.innerHTML = vm.vmType?.toString() ?? "Unknown";
+        vmRow.appendChild(vmType);
+
+        const performance = document.createElement('td');
+        performance.innerHTML = vm.performance?.toString() ?? "Unknown";
+        vmRow.appendChild(performance);
+
+        const publicIp = document.createElement('td');
+        publicIp.innerHTML = vm.publicIp?.toString() ?? "Unknown";
+        vmRow.appendChild(publicIp);
+    }
+    return result;
+}
